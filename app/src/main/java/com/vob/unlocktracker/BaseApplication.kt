e4b5c831c5bdd4bc24b71b27a265d72f965da936
aditya <mishraaditya595@gmail.com>
2021-03-11 23:04:32 +0530

    began di implementation

---
+++ app/src/main/java/com/vob/unlocktracker/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.vob.unlocktracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+}
---
+++ app/src/main/java/com/vob/unlocktracker/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.vob.unlocktracker.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+}
