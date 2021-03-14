44231d2c7b44d6f006adfb213b5771be5fcb28d8
aditya <mishraaditya595@gmail.com>
2021-03-14 20:55:58 +0530

    handled location permissions

--- app/src/main/java/com/vob/unlocktracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/vob/unlocktracker/ui/fragments/RunFragment.kt
@@ -1,5 +1,6 @@
 package com.vob.unlocktracker.ui.fragments
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -8,10 +9,15 @@ import androidx.navigation.fragment.findNavController
 import com.vob.unlocktracker.R
 import com.vob.unlocktracker.databinding.FragmentRunBinding
 import com.vob.unlocktracker.ui.viewmodel.MainViewModel
+import com.vob.unlocktracker.util.Constants.REQUEST_CODE_LOCATION_PERMISSIONS
+import com.vob.unlocktracker.util.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import java.util.jar.Manifest
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: FragmentRunBinding
 
@@ -21,8 +27,53 @@ class RunFragment : Fragment(R.layout.fragment_run) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRunBinding.bind(view)
 
+        requestPermissions()
+
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
     }
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermission(requireContext()))
+            return
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "You need to accept location permissions to use this app.",
+                    REQUEST_CODE_LOCATION_PERMISSIONS,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "You need to accept location permissions to use this app.",
+                    REQUEST_CODE_LOCATION_PERMISSIONS,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
+
--- app/src/main/java/com/vob/unlocktracker/util/Constants.kt
+++ app/src/main/java/com/vob/unlocktracker/util/Constants.kt
@@ -2,4 +2,5 @@ package com.vob.unlocktracker.util
 
 object Constants {
     const val RUNNING_DATABASE_NAME = "running_db"
+    const val REQUEST_CODE_LOCATION_PERMISSIONS = 1000
 }
