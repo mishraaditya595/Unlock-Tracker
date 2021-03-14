692718e74d6a1ea125a9ab2efae7269386867a51
aditya <mishraaditya595@gmail.com>
2021-03-14 22:17:04 +0530

    implemented map view

--- app/src/main/java/com/vob/unlocktracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/vob/unlocktracker/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,66 @@
 package com.vob.unlocktracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.vob.unlocktracker.R
+import com.vob.unlocktracker.databinding.FragmentTrackingBinding
 import com.vob.unlocktracker.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
+    lateinit var binding: FragmentTrackingBinding
+
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentTrackingBinding.bind(view)
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
 }
