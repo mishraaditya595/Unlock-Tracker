daf9e88d7781c319c6433a072e392a5ee52c0724
aditya <mishraaditya595@gmail.com>
2021-03-16 12:57:00 +0530

    User location tracking implemented

--- app/src/main/java/com/vob/unlocktracker/service/TrackingService.kt
+++ app/src/main/java/com/vob/unlocktracker/service/TrackingService.kt
@@ -1,5 +1,6 @@
 package com.vob.unlocktracker.service
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
@@ -7,25 +8,59 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
-import android.os.Build.VERSION.SDK_INT
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.vob.unlocktracker.R
 import com.vob.unlocktracker.ui.MainActivity
 import com.vob.unlocktracker.util.Constants.ACTION_PAUSE_SERVICE
 import com.vob.unlocktracker.util.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.vob.unlocktracker.util.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.vob.unlocktracker.util.Constants.ACTION_STOP_SERVICE
+import com.vob.unlocktracker.util.Constants.FASTEST_LOCATION_INTERVAL
+import com.vob.unlocktracker.util.Constants.LOCATION_UPDATE_INTERVAL
 import com.vob.unlocktracker.util.Constants.NOTIFICATION_CHANNEL_ID
 import com.vob.unlocktracker.util.Constants.NOTIFICATION_CHANNEL_NAME
 import com.vob.unlocktracker.util.Constants.NOTIFICATION_ID
+import com.vob.unlocktracker.util.TrackingUtility
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService: LifecycleService() {
 
     var isFirstRun = true
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -56,7 +91,65 @@ class TrackingService: LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking)
+        {
+            if (TrackingUtility.hasLocationPermission(this))
+            {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                        request,
+                        locationCallback,
+                        Looper.getMainLooper()
+                )
+            }
+        }
+        else
+        {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+            if (isTracking.value!!)
+            {
+                result?.locations?.let { locations ->
+                    for (location in locations)
+                    {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos) //to add at end of the list
+                pathPoints.postValue(this)
+            }
+
+        }
+    }
+
+    private fun addEmptyPolyLines() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService() {
+        addEmptyPolyLines()
+        isTracking.postValue(true)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
